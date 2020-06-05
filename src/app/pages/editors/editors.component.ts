49b2fda58471d867ab25ea3bdbcf59ff8563bd33
Bitseat <bitseatt@gmail.com>
2020-06-05 16:19:03 +0300

    pushing project

---
+++ src/app/pages/editors/editors.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-editors',
+  styleUrls: ['./editors.component.scss'],
+  templateUrl: './editors.component.html',
+})
+export class EditorsComponent {
+
+}
